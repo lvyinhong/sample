5c9fae9a45c4283d50ec431a7cc82633d50d66d4
lvyinhong <1903238820@qq.com>
2018-11-14 14:59:27 +0000

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,8 +59,51 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+                    ->with('user')
+                    ->orderBy('created_at', 'desc');
     }
 
+    /**
+     *  一个用户拥有多个粉丝
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     *  一个用户关注多个人
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     *
+     *
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
